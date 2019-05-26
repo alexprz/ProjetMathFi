742a22c0248454e06b726dc6b8d808b4304bc70c
Unknown <alexperez@wanadoo.fr>
2019-05-26 18:19:11 +0200

    Added simulate_St

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-import numpy as numpy
+import numpy as np
 
+# Parameters
+T = 1.
+r = 0.03
+mu = -0.05
+sigma = 0.05
+
+def simulate_St(S0, dt):
+    n = int(T/dt)+1
+    St = np.zeros(n)
+    St[0] = S0
+
+    for k in range(1, n):
+        eps = np.random.normal()
+        St[k] = St[k-1]*(1+mu*dt + np.sqrt(dt)*eps)
+
+    return St
 
 if __name__ == '__main__':
-    print('hello')
+    print(simulate_St(10, 0.1))
